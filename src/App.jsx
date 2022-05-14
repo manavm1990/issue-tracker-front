e999f81107d1a1032a7cfcd8b992f91140269534
Manav Misra <manavm@visionwebsoft.com>
2022-05-13 21:56:32 -0500

    feat: :sparkles: render first query
    Includes loading and error.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,23 @@
+import { useQuery } from "react-query";
+import apiService from "./api.service";
+import Main from "./components/Main";
+
 export default function App() {
-  return <h1 className="font-black my-3 text-8xl text-center">Hello World</h1>;
+  const userQuery = useQuery(["manavm1990"], () => apiService.show());
+
+  const { data, isLoading, isError } = userQuery;
+
+  function getChildren() {
+    if (isLoading) {
+      return <p className="text-orange-500">Loading...</p>;
+    }
+
+    if (isError) {
+      return <p className="text-red-500">Error</p>;
+    }
+
+    return <p className="text-bold">{data?.login}</p>;
+  }
+
+  return <Main>{getChildren()}</Main>;
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import PropTypes from "prop-types";
+
+export default function Main({ children }) {
+  return (
+    <main className="flex h-screen justify-center items-center">
+      {children}
+    </main>
+  );
+}
+
+Main.propTypes = {
+  children: PropTypes.element.isRequired,
+};
