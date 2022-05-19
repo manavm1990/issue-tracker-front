4d0d8fea85079f49b9b9424b54e4f2fd44638127
Manav Misra <manavm@visionwebsoft.com>
2022-05-19 18:11:35 -0500

    fix: :label: give assignee a shape

--- src/types/Issue.js
+++ src/types/Issue.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 export default PropTypes.shape({
   title: PropTypes.string.isRequired,
   number: PropTypes.number.isRequired,
-  assignee: PropTypes.string,
+  assignee: PropTypes.shape({ login: PropTypes.string.isRequired }),
   comments: PropTypes.number.isRequired,
   user: PropTypes.shape({ login: PropTypes.string.isRequired }).isRequired,
   created_at: PropTypes.string.isRequired,
